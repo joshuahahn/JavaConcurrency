bbdd7b02b9ee5ab6e4aaed0a419b2ebbf2208fc8
joshuahahn <joshua.hahnjy@gmail.com>
2022-10-27 19:30:54 -0400

    10.27

--- Main.java
+++ Main.java
@@ -4,8 +4,28 @@ public class Main implements Runnable {
         System.out.println("This is inside a thread!");
     }
 
-    public static void main(String[] args) {
-        Thread myThread = new Thread(new Main());
+    public static void main(String[] args) throws InterruptedException {
+        String[] list = {
+            "One",
+            "Two",
+            "Three",
+            "Four"
+        };
+
+        for (String word: list) {
+            Thread.sleep(4000);
+            System.out.println(word);
+        }
+
+        /* 
+        // Runnable object
+        Runnable myRunnable = new Main();
+        // Thread object
+        Thread myThread = new Thread(myRunnable);
+
+        // Note that the run() method is never called. It is implicitly called
+        // when the thread containing the Main runnable is started.
         myThread.start();
+        */
     }
 }
